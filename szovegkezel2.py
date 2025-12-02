ec8ed362a7088defc06bf8bc948a38136e65ef4a
Béres Olivér◘ <beresoli07@gmail.com>
2025-12-02 11:37:01 +0100

    váááóó

--- szovegkezel2.py
+++ szovegkezel2.py
@@ -15,10 +15,33 @@ print(szöv)
 
 #2. feladat
 szamdb = 0
-print(string.digits)
+#print(string.digits)
 
 for karakter in szöv:
     if karakter in string.digits:
         szamdb += 1
         
-print(szamdb)
+print("A szám karakterek száma: ",szamdb)
+
+#3. feladat
+print(string.punctuation)
+
+speclista=[]
+
+for karakter in szöv:
+    if karakter in string.punctuation and karakter not in speclista:
+        speclista.append(karakter)
+
+print(speclista)
+
+try:
+    with open("string_output.txt","w",encoding = 'utf-8')as fajl:
+        
+        
+        for i in range(len(speclista)):
+            fajl.write(speclista[i])
+            if i < len(speclista)-1:
+                fajl.write(", ")
+        
+except IOError as ex:
+    print(ex)
